55954a624ae1c9de5bbfa37188dd55434ba5fd8e
Noah <noahcuroe@gmail.com>
2019-12-18 22:31:01 -0600

    Update constants
    Former-commit-id: 624896d5f3cbaa6096c04beef39183bfa7db1baa

--- SSECommon/Constants.cs
+++ SSECommon/Constants.cs
@@ -4,12 +4,9 @@ using System.Text;
 
 namespace SSECommon {
     public static class Constants {
-        public const string OFFLINE_SCORING_CONFIGURATION_PASSWORD = "SSE_CONFIGURATION_PASSWORD";
+        public const string OFFLINE_SCORING_CONFIGURATION_PASSWORD = "XuYm48yuEcH8LMnQtKQvgvXpFt5Bq2AP";
 
         public const string KEY_EXCHANGE_SANITY_CHECK = "KEY EXCHANGE SUCCESS";
-
-        public const string CRYPTO_API_WINDOWS = "CNG";
-        public const string CRYPTO_API_UNIX = "OPENSSL";
 
         public const string SCORING_PROCESS_START = "START SCORING PROCESS";
         public const string SCORING_PROCESS_CONTINUE = "CONTINUE SCORING PROCESS";
